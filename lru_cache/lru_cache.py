b377700eef8b46ee966d70437a4a482a2c5f0c20
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2019-12-18 15:31:47 -0500

    commiting for Wednessdays assignment

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,13 @@
+from doubly_linked_list import DoublyLinkedList
+
+## Add to head if most recently used item. 
+
+## Subtract from tail IF the amount of data exeeds 
+# the one of the cache
+
+## IF the key itself is already in cache. Need to move 
+# that entry to the head of the list.
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +17,10 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        self.size = 0
+        self.order = DoublyLinkedList()
+        self.storage = dict()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +29,15 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+    #Needs to lookup the key and see if its in the cache.
+    #Add to head if most recently used item.
     def get(self, key):
-        pass
+        if key in self.storage:
+            node = self.storage[key]
+            self.order.move_to_front(node)
+            return node.value[1]
+        else:
+            return None
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +49,27 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+    #Needs to lookup the key to see if its in hte table. 
+    # If in table move the entry to the head of the DLL
+    # If > limit needs to remove from the tail of the DLL
     def set(self, key, value):
-        pass
+
+        # if key in cache
+        if key in self.storage:
+            node = self.storage[key]
+            node.value = (key, value)
+            self.order.move_to_front(node)
+            return
+
+
+        # if in cache move to front & update
+        if self.size == self.limit:
+            del self.storage[self.order.tail.value[0]]
+            self.order.remove_from_tail()
+            self.size -= 1
+
+        # Defining head as most recent and tail is oldest
+        self.order.add_to_head((key, value))
+        self.storage[key] = self.order.head
+        self.size += 1
+
